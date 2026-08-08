36a4322074103cb0ae17db8f8b0358442d81fc12
ElHombreZ <kaypro1932@gmail.com>
2026-08-08 15:43:21 -0600

    Delete compiler generated files that are not linking well or generally can be regenerated again

--- boards/bms/Debug/ADBMS6830/lib/src/adBms6830CmdList.d
+++
@@ -1,6 +0,0 @@
-ADBMS6830/lib/src/adBms6830CmdList.o: \
- ../ADBMS6830/lib/src/adBms6830CmdList.c \
- /Users/ishanchitale/STM32CubeIDE/workspace_1.16.0/mk11-bms-mcu/ADBMS6830/lib/inc/adBms6830CmdList.h \
- /Users/ishanchitale/STM32CubeIDE/workspace_1.16.0/mk11-bms-mcu/ADBMS6830/program/inc/common.h
-/Users/ishanchitale/STM32CubeIDE/workspace_1.16.0/mk11-bms-mcu/ADBMS6830/lib/inc/adBms6830CmdList.h:
-/Users/ishanchitale/STM32CubeIDE/workspace_1.16.0/mk11-bms-mcu/ADBMS6830/program/inc/common.h:
