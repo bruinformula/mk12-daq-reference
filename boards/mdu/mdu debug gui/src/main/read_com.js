63d86eff3cb3597ae2d9c63417f8ffc35e701101
ElHombreZ <kaypro1932@gmail.com>
2026-08-13 20:26:51 -0600

    Remove copy of each board and vendor generated drivers

--- boards/mdu/mdu debug gui/src/main/read_com.js
+++
@@ -1,8 +0,0 @@
-const { SerialPort } = require('serialport');
-const port = new SerialPort({ path: 'COM31', baudRate: 115200 });
-port.on('data', (chunk) => {
-  console.log('HEX:', chunk.toString('hex'));
-  console.log('TXT:', chunk.toString('utf8'));
-  process.exit(0);
-});
-setTimeout(() => { console.log('Timeout'); process.exit(1); }, 3000);
